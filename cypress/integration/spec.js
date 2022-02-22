bb2712416c28007c450ca21a7bdd6fc672ac1155
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-02-22 16:26:58 -0500

    fixed

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 it('adds todos', () => {
-  cy.request('POST', '/todos', { todos: [] })
+  cy.request('POST', '/reset', { todos: [] })
   cy.visit('/')
   cy.get('.new-todo').type('Learn Cypress{enter}').type('Write tests{enter}')
   cy.get('.todo').should('have.length', 2)
